c3e50cb46901b10f5114f2b441cd4404604da8ad
Winston Chu <wpchu@andrew.cmu.edu>
2017-03-01 10:56:35 -0500

    got rid of paginate for item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,11 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @boards = Item.active.for_category('boards').alphabetical.paginate(page: params[:page]).per_page(10)
-    @pieces = Item.active.for_category('pieces').alphabetical.paginate(page: params[:page]).per_page(10)
-    @clocks = Item.active.for_category('clocks').alphabetical.paginate(page: params[:page]).per_page(10)
-    @supplies = Item.active.for_category('supplies').alphabetical.paginate(page: params[:page]).per_page(10)
-    @inactive_items = Item.inactive.alphabetical.paginate(page: params[:page]).per_page(10)
+    @boards = Item.active.for_category('boards').alphabetical
+    @pieces = Item.active.for_category('pieces').alphabetical
+    @clocks = Item.active.for_category('clocks').alphabetical
+    @supplies = Item.active.for_category('supplies').alphabetical
+    @inactive_items = Item.inactive.alphabetical
   end
 
   def new
