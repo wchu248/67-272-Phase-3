c9db7b94a0ee23d4c57ea9da8099398798085ff0
Winston Chu <wpchu@andrew.cmu.edu>
2017-03-02 12:38:17 -0500

    passed more cucumber tests

--- app/controllers/item_prices_controller.rb
+++ app/controllers/item_prices_controller.rb
@@ -10,6 +10,7 @@ class ItemPricesController < ApplicationController
 
   def create
     @item_price = ItemPrice.new(item_price_params)
+    @item_price.start_date = Date.today
     if @item_price.save
         # if saved to database
         @item = @item_price.item
