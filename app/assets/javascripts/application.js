8e9e4f3a8628627ce03f4436d2a95aa81bbd75d9
Winston Chu <wpchu@andrew.cmu.edu>
2017-03-01 22:26:44 -0500

    bunch of view stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require_tree .
 
 $(function() {
   $(document).foundation('topbar', 'reflow');
 });
+
+$(function(){ $(document).foundation(); });
